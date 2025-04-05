394cd96dc4cb7561826651cc9f056d0035c21cc1
Bailly Rurangirwa <rubailly@gmail.com>
2025-04-05 15:41:44 +0200

    chore: Update server configuration with new IP and credentials

--- config/server-config.sh
+++ config/server-config.sh
@@ -4,7 +4,7 @@
 # Change these values to match your server IPs
 MASTER_IP="157.245.39.216"
 WORKER1_IP="38.242.250.17"
-WORKER2_IP="45.90.120.29"
+WORKER2_IP="worker2"
 
 # Hostname configuration
 MASTER_HOSTNAME="cm-master"
@@ -12,5 +12,5 @@ WORKER1_HOSTNAME="worker1"
 WORKER2_HOSTNAME="worker2"
 
 # SSH configuration
-SSH_USER="root"
-SSH_PASSWORD="your_secure_password"
+SSH_USER="admin@Log11c"
+SSH_PASSWORD="aetrade123"
